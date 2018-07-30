5b38c4e28e4cb9f748f9fee69204b47281fd8a43
MuntasirAnik <anik.muntasir005@gmail.com>
2018-07-30 04:16:25 +0000

    classwork9

--- server.js
+++ server.js
@@ -5,27 +5,42 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 var server = http.Server(app);
-var mongo = require('mongodb');
+//var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://" + process.env.IP + ":27017/test");
+
+mongoose.connection.on('erroe',function(){
+  console.log('Could not connect to mongodb');
+});
+//define schema with mongoose
+var userSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: "Name is required"
+  } ,
+  email:String
+});
+var User = mongoose.model('User',userSchema);
 //for local
 //var db, url = "mongodb://localhost:27071";
 //for c9
-var db, url = 'mongodb://' + process.env.IP + ":27017/test";
-mongo.MongoClient.connect(url,
-            {useNewUrlParser:true},
-            function(err,client){
-              if(err){
-                console.log(err);
-                console.log("Could not connect to mongoDB");
-              }else{
-                db= client.db('node=cwD');
-              }
-            });
-            
-var save = function(from_data){
-  db.createCollection('users',function(err, collection){});
-  var collection= db.collection('users');
-  collection.save(from_data);
-};            
+//var db, url = 'mongodb://' + process.env.IP + ":27017/test";
+//mongo.MongoClient.connect(url,
+ //           {useNewUrlParser:true},
+ //           function(err,client){
+ //             if(err){
+  //              console.log(err);
+  //              console.log("Could not connect to mongoDB");
+ //             }else{
+ //               db= client.db('node=cwD');
+//              }
+//            });
+//            
+//var save = function(from_data){
+ // db.createCollection('users',function(err, collection){});
+//  var collection= db.collection('users');
+//  collection.save(from_data);
+//};            
 
 
 
@@ -41,7 +56,15 @@ app.get('/email',function(req,res){
 app.post('/submit_user',function(req,res)
 {
     console.log(req.body);
-    save(req.body);
+    var new_user = new User(req.body);
+    new_user.save(function(err,data){
+      if(err){
+        return res.status(400)
+                  .json({message:"couldn't save user"})
+      }
+      res.status(200).json(data);
+    })
+    //save(req.body);
     res.status(200);
 });
   server.listen(process.env.PORT, process.env.IP, function(){
